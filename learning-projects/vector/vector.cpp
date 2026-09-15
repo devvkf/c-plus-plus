33015119f2310480c7bc5234c2d6adcd69078569
devvkf <benedictus.marcello@gmail.com>
2026-09-15 17:24:01 +0700

    bugfix: fixed bug that truncated doubles in vector.cpp

--- learning-projects/vector/vector.cpp
+++ learning-projects/vector/vector.cpp
@@ -7,11 +7,11 @@ using std::cout;
 int main() {
     vector<double> decimalDatabase;
 
-    decimalDatabase.push_back(1.0);
-    decimalDatabase.push_back(2.0);
+    decimalDatabase.push_back(1.1);
+    decimalDatabase.push_back(2.2);
     decimalDatabase.push_back(3.0);
 
-    for (int fp : decimalDatabase) {
+    for (const double fp : decimalDatabase) {
         cout << fp << '\n';
     }
 
