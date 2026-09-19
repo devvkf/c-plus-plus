ccb05f7fda55b1619fdb9f500090e1116b9e83a4
devvkf <benedictus.marcello@gmail.com>
2026-09-19 22:08:34 +0700

    add: lambda project

--- learning-projects/lambda/lambda.cpp
+++ learning-projects/lambda/lambda.cpp
@@ -1,3 +1,18 @@
-//
-// Created by bened on 9/19/2026.
-//
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int main() {
+    vector<double> doubleDB = {15.2, 32.3, 98.9};
+
+    std::sort(doubleDB.begin(), doubleDB.end()
+        , [](const int d, const int e) {
+            return d > e;
+        });
+
+    for (auto& item : doubleDB) {
+        std::cout << item << "\n";
+    }
+}
