16e67710f4b059dc86312c0d4dc29bbff4271c85
devvkf <benedictus.marcello@gmail.com>
2026-09-20 15:59:26 +0700

    fix: fix fp_checker app

--- app-projects/fp_checker/isDouble.cpp
+++ app-projects/fp_checker/isDouble.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <concepts>
+#include <string>
 
 using std::cout;
 
@@ -12,16 +13,21 @@ static void checkValue(D value)
     cout << "Passed: " << value << '\n';
 }
 
+bool is_decimal_string(const std::string& str) {
+    return str.find('.') != std::string::npos;  // has a decimal point
+}
+
 int main() {
     cout << "==== float checker ====\n"
             "value to check:\n";
-    double valueToCheck{};
-    std::cin >> valueToCheck;
+    std::string input;
+    std::cin >> input;
 
-    if (std::cin >> valueToCheck) {
+    if (is_decimal_string(input)) {
+        double valueToCheck = std::stod(input);
         checkValue(valueToCheck);
     }
     else {
-        cout << "error: invalid input, not a double.\n";
+        cout << "error: not a decimal number.\n";
     }
 }
